f758841cb48fec5b4f270a3e2447cf274e180380
Markus Marandi <markusmarandi@gmail.com>
2025-01-16 21:40:56 +0100

    update setup.py for packaging

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="gvcf-vds-combiner",
+setup(
+    name="gvcf_to_vds_pipeline",
     version="0.1.0",
+    description="A Python package for converting GVCFs to Hail VariantDataset (VDS)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Markus Marandi",
     author_email="markus.marandi@ut.ee",
-    description="Combine GVCFs into a Hail VariantDataset (VDS).",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/OligoGeneticDiseases/gvcf-to-vds-pipeline.git",
-    packages=setuptools.find_packages(),
-    python_requires=">=3.7",
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "hail>=0.2.133"
+        "pyspark",
     ],
+    entry_points={
+        "console_scripts": [
+            "gvcf-to-vds=gvcf_to_vds_pipeline.__main__:main",
+        ],
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Creative Commons Attribution Non Commercial Share Alike License",
     ],
+    python_requires=">=3.7",
 )
